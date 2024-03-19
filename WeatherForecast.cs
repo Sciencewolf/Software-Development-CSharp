4959367adec968d99bfbb2e2b08f54e05165c6e3
Márton Áron <aron17marton@gmail.com>
2024-03-19 21:06:14 +0100

    Removed wf.cs file

--- WeatherForecast.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Autószerelő
-{
-    public class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
-        public int TemperatureC { get; set; }
-        public string Summary { get; set; }
-    }
-}
